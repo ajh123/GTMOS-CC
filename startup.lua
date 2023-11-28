9712671ffb89e7a7a62fbb7df63cfb2bfd135bd2
ajh123 <ajh123@users.noreply.github.com>
2023-11-28 15:39:22 +0000

    Add docs and use coroutines

--- startup.lua
+++ startup.lua
@@ -1,6 +1,9 @@
-require "api.sandbox"
+local sandbox = require "api.sandbox"
 
 -- Your Lua program file
 local programFilePath = shell.resolve("programs/shell/main.lua")
-local func = makeProgram(programFilePath)
-runProgram(func)
+local func = sandbox.makeProgramFile(programFilePath)
+local success, result = coroutine.resume(func)
+if not success then
+    print("Error running program: " .. tostring(result))
+end
