865efd102fa2904dda180850ca475bb1fba37d11
ajh123 <ajh123@users.noreply.github.com>
2023-11-29 17:07:39 +0000

    Fix links to print

--- api/virtual_env.lua
+++ api/virtual_env.lua
@@ -7,7 +7,7 @@ local virtual_env = {}
 
 --- Prints a message to the console.
 --
--- This is the GT MOS - CC speficic version of [`print`], it behaves excatly like the Lua bulitin [`print`].
+-- This is the GT MOS - CC speficic version of [`print`](https://www.lua.org/manual/5.1/manual.html#pdf-print), it behaves excatly like the Lua bulitin [`print`](https://www.lua.org/manual/5.1/manual.html#pdf-print).
 --
 -- @tparam any ... The data to print.
 -- @since 0.1.0
